c223b49b6641350a396f19bb0506c33900283212
Ben Hegarty <ben@elementice.com>
2019-04-25 16:24:12 +1000

    Make it quieter :P

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -9,6 +9,6 @@ time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 #echo "Updating configuration"
 #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
-((cd ~/dotfiles && git pull && git submodule update --init --recursive) &) > /dev/null
+((cd ~/dotfiles && git pull > /dev/null && git submodule update --init --recursive > /dev/null) &) > /dev/null
 source ~/dotfiles/zsh/zshrc.sh
 
